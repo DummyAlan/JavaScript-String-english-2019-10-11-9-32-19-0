74425a83e8e0eb6cd7d0d1097506f240e0b7a1c8
Alan Wan <alan.wan@oocl.com>
2022-11-24 11:08:44 +0800

    finished requirement

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+name = name.toUpperCase();
+console.log(name);
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+let splitSentence = sentence.split(" ").map(str => str[0].toUpperCase()+str.substring(1)).join(" ");
+
+console.log(splitSentence);
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var number = Number(money.replace(/[^0-9.-]+/g,"")).valueOf();
+console.log(number);
